00e5597e7b04ae9804f673a1393ad9328d4983e4
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-05-01 13:48:58 -0500

    segunda corrida de eslint para formato de JS

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,18 +1,18 @@
-const StudentController = require('../../lib/controllers/StudentController');
+const StudentController = require("../../lib/controllers/StudentController");
 
-describe('Student Controller Tests', () => {
-  test('1. Get all Students', () => {
-    const test = StudentController.getAllStudents();
-    expect(test[0].name).toBe('Warren');
-  });
+describe("Student Controller Tests", () => {
+    test("1. Get all Students", () => {
+        const test = StudentController.getAllStudents();
+        expect(test[0].name).toBe("Warren");
+    });
 
-  test('2. Get email of certified students', () => {
-    const test = StudentController.getEmailOfCertifiedStudents();
-    expect(test[0]).toBe('Todd@visualpartnership.xyz');
-  });
+    test("2. Get email of certified students", () => {
+        const test = StudentController.getEmailOfCertifiedStudents();
+        expect(test[0]).toBe("Todd@visualpartnership.xyz");
+    });
 
-  test('3. Get name of students with more than 500 credits', () => {
-    const test = StudentController.getStudentsOver500();
-    expect(test[0]).toBe('Warren');
-  });
+    test("3. Get name of students with more than 500 credits", () => {
+        const test = StudentController.getStudentsOver500();
+        expect(test[0]).toBe("Warren");
+    });
 });
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,8 +1,8 @@
-const Reader = require('../../lib/utils/Reader');
+const Reader = require("../../lib/utils/Reader");
 
-describe('Read a JSON file', () => {
-  test('1. Reader.js  wth data', () => {
-    const test = Reader.readJsonFile('./visualpartners.json');
-    expect(test[0].name).toBe('Warren');
-  });
+describe("Read a JSON file", () => {
+    test("1. Reader.js  wth data", () => {
+        const test = Reader.readJsonFile("./visualpartners.json");
+        expect(test[0].name).toBe("Warren");
+    });
 });
