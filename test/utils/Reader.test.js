ab8f53f932b91fcf9d24142633e91d7ada040bbb
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-05-01 10:39:40 -0500

    creacion de reader.js con prueba unitaria exitosa

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("./../../lib/utils/Reader");
+
+describe("Read a JSON file", () => {
+    test("1. Reader.js  wth data", () => {
+        const test = Reader.readJsonFile("./visualpartners.json");
+        expect(test[0].name).toBe("Warren");
+    });
+});
